3a21ddebd6fa9339b9e8a8f021dda8c98a55461c
AudriusLunys <lunysaudrius@inbox.lt>
2020-08-07 14:20:44 +0300

    DAO working, added input scenes,  adding display scenes

--- src/main/java/studentmanager/FrontEnd/Menus/FacultyMenu.java
+++ src/main/java/studentmanager/FrontEnd/Menus/FacultyMenu.java
@@ -3,13 +3,16 @@ package studentmanager.FrontEnd.Menus;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import net.bytebuddy.build.Plugin;
 import studentmanager.dao.FacultyDAO;
 import studentmanager.domain.Faculty;
 
@@ -18,6 +21,8 @@ import java.util.List;
 public class FacultyMenu extends Application {
     Stage facultyWindow;
     TableView<Faculty> facultyTable ;
+    TextField facultyNameInput, facultySpecializationInput;
+
 
     public static void main(String[] args) {
       //for test purposes
@@ -46,6 +51,7 @@ public class FacultyMenu extends Application {
       facultyWindow = primaryStage;
       facultyWindow.setTitle("Faculty Menu");
 
+
         TableColumn<Faculty,Integer> facultyIdColumn = new TableColumn<>("Faculty ID");
         facultyIdColumn.setMinWidth(100);
         facultyIdColumn.setCellValueFactory(new PropertyValueFactory<>("facultyId"));
@@ -58,13 +64,36 @@ public class FacultyMenu extends Application {
         facultySpecializationColumn.setMinWidth(200);
         facultySpecializationColumn.setCellValueFactory(new PropertyValueFactory<>("specialization"));
 
+        facultyNameInput = new TextField();
+        facultyNameInput.setPromptText("Faculty Name");
+        facultyNameInput.setMinWidth(100);
+
+        facultySpecializationInput = new TextField();
+        facultySpecializationInput.setPromptText("Faculty Specialization");
+        facultySpecializationInput.setMinWidth(100);
+
+        Button addButton = new Button("Add");
+        addButton.setOnAction(event -> addButtonClicked());
+
+        Button deleteButton = new Button("Delete");
+       // deleteButton.setOnAction(event -> deleteButtonClicked);
+        Button editButton = new Button("Edit");
+       // editButton.setOnAction(event -> editButtonClicked);
+
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(10,10,10,10));
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(facultyNameInput,facultySpecializationInput,addButton,deleteButton,editButton);
+
+
         facultyTable = new TableView<>();
         facultyTable.setItems(getFaculty());
         facultyTable.getColumns().addAll(facultyIdColumn,facultyNameColumn,facultySpecializationColumn);
 
 
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(facultyTable);
+        vBox.getChildren().addAll(facultyTable,hBox);
         Scene scene = new Scene(vBox);
         facultyWindow.setScene(scene);
         facultyWindow.show();
@@ -78,5 +107,19 @@ public class FacultyMenu extends Application {
         for (Faculty faculty : facultyList) {
             obsFaculties.add(faculty);
         }return  obsFaculties;
+    }
+
+    public void addButtonClicked () {
+        Faculty faculty = new Faculty();
+        faculty.setFacultyName(facultyNameInput.getText());
+        faculty.setSpecialization(facultySpecializationInput.getText());
+        FacultyDAO facultyDAO = new FacultyDAO();
+        facultyDAO.addFaculty(faculty);
+        facultyNameInput.clear();
+        facultySpecializationInput.clear();
+
+    }
+    public void refreshTable() {
+        facultyTable.refresh();
     }
 }
