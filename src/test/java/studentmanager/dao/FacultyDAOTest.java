2c63e44a4284810d1ddded254b699dc03a3b54f1
AudriusLunys <lunysaudrius@inbox.lt>
2020-08-18 13:34:28 +0300

    Main functionality finished

---
+++ src/test/java/studentmanager/dao/FacultyDAOTest.java
@@ -0,0 +1,26 @@
+package studentmanager.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacultyDAOTest {
+
+    @org.junit.jupiter.api.Test
+    void addFaculty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFaculty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFaculty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateFaculty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFacultyList() {
+    }
+}
