46adc5c2373fba39febf98c38ff30cb3ea469a91
AudriusLunys <lunysaudrius@inbox.lt>
2020-08-19 11:45:23 +0300

    Junit tests started

--- src/test/java/studentmanager/dao/FacultyDAOTest.java
+++ src/test/java/studentmanager/dao/FacultyDAOTest.java
@@ -1,26 +1,72 @@
 package studentmanager.dao;
 
+import studentmanager.domain.Faculty;
+
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FacultyDAOTest {
 
+    private FacultyDAO facultyDAO = new FacultyDAO();
+
     @org.junit.jupiter.api.Test
     void addFaculty() {
+        Faculty faculty = new Faculty();
+        faculty.setFacultyName("TestFaculty");
+        faculty.setSpecialization("TestSpec");
+        facultyDAO.addFaculty(faculty);
+        assertNotNull(faculty);
+        assertEquals(faculty.getFacultyName(),"TestFaculty");
+        assertEquals(faculty.getSpecialization(),"TestSpec");
+
     }
 
     @org.junit.jupiter.api.Test
     void getFaculty() {
+        Faculty faculty = new Faculty();
+        faculty.setFacultyName("OneFaculty");
+        faculty.setSpecialization("BestSpec");
+        facultyDAO.addFaculty(faculty);
+        assertEquals(faculty.getFacultyId(),1);
     }
 
     @org.junit.jupiter.api.Test
     void removeFaculty() {
+        Faculty faculty = new Faculty();
+        faculty.setFacultyName("Test");
+        faculty.setSpecialization("TestTest");
+        assertNotNull(faculty);
+        assertEquals(faculty.getFacultyName(),"Test");
+        if (faculty.getFacultyId()!=null) {
+            faculty = new Faculty();
+            faculty.setFacultyName("");
+            faculty.setSpecialization("");
+            facultyDAO.updateFaculty(faculty);
+            assertNull(faculty);
+            assertEquals(faculty.getFacultyName(), "");
+        }
+
     }
 
     @org.junit.jupiter.api.Test
     void updateFaculty() {
+        Faculty faculty = new Faculty();
+        faculty.setFacultyName("One");
+        faculty.setSpecialization("SpecialOne");
+        assertNotNull(faculty);
+        if (faculty.getFacultyName()=="One") {
+            faculty = new Faculty();
+            faculty.setFacultyName("OneTwo");
+            faculty.setSpecialization("SpecialOneTwo");
+            facultyDAO.updateFaculty(faculty);
+            assertNotNull(faculty);
+            assertEquals(faculty.getFacultyName(),"OneTwo");
+        }
     }
 
     @org.junit.jupiter.api.Test
     void getFacultyList() {
+
+        }
     }
-}
